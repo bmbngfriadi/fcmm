caf5fee375a2a9fec50af35b5809f093692bc752
Bambang Friadi <bambang.friadi@gmail.com>
2026-09-19 08:30:14 +0700

    chore: remove instances config to force PM2 fork mode

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,6 @@ module.exports = {
       script: "node_modules/next/dist/bin/next",
       args: "start -p 3000",
       cwd: "./",
-      instances: 1,
       exec_mode: "fork",
       autorestart: true,
       watch: false,
