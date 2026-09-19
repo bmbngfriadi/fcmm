40cae3f672dc71cdc646bd1d1bad8f5a8af0402a
Bambang Friadi <bambang.friadi@gmail.com>
2026-09-19 08:35:10 +0700

    fix(pm2): revert config to prevent 502

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,11 +5,11 @@ module.exports = {
       script: "node_modules/next/dist/bin/next",
       args: "start -p 3000",
       cwd: "./",
-      exec_mode: "fork",
+      instances: 1,
       autorestart: true,
       watch: false,
-      max_memory_restart: "400M",
-      node_args: "--max-old-space-size=256",
+      max_memory_restart: "1G",
+      node_args: "--max-old-space-size=400",
       env: {
         NODE_ENV: "production",
       }
